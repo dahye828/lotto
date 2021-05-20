c966195ec50d9a356d6a16eb6376ac67e9111bc4
dahye828 <reg0828@naver.com>
2021-05-20 14:38:44 +0900

    Open NameActivity

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -11,9 +11,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val constellcardView = findViewById<CardView>(R.id.constellationcard)
+        val namecardView = findViewById<CardView>(R.id.namecard)
 
         constellcardView.setOnClickListener{
             startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
+        }
+        namecardView.setOnClickListener {
+            startActivity(Intent(this@MainActivity, NameActivity::class.java))
         }
     }
 }
